69775ec222acb49516e1cb93da7e1ad58ec17e87
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-07-07 19:34:06 +0600

    Expo Setup

---
+++ job-search-app/app/index.js
@@ -0,0 +1,14 @@
+import {View, Text, ScrollView, SafeAreaView} from 'react-native';
+import { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+const Home = () => {
+    return(
+        <View>
+            <Text>
+                Home
+            </Text>
+        </View>
+    )
+}
+
+export default Home;
