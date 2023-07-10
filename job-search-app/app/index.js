f403133c4e59838283bfe9d358220dee717aa2de
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-07-10 16:09:35 +0600

    Home Screen using TouchableOpacity, FlatList & ActivityIndicator

--- job-search-app/app/index.js
+++ job-search-app/app/index.js
@@ -1,13 +1,33 @@
-import {View, Text, ScrollView, SafeAreaView} from 'react-native';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
 import { useState } from 'react';
 import { Stack, useRouter } from 'expo-router';
+import { COLORS, icons, images, SIZES } from '../constants';
+import {
+    Nearbyjobs,
+    Popularjobs, ScreenHeaderBtn, Welcome
+} from '../components';
 const Home = () => {
-    return(
-        <View>
-            <Text>
-                Home
-            </Text>
-        </View>
+    const router = useRouter();
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen options={{
+                headerStyle: { backgroundColor: COLORS.lightWhite },
+                headerShadowVisible: false,
+                headerLeft: () => (<ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"></ScreenHeaderBtn>),
+                headerRight: () => (<ScreenHeaderBtn iconUrl={images.profile} dimension="100%"></ScreenHeaderBtn>),
+                headerTitle: "Job Search App",
+                headerTitleAlign:'center'
+            }}>
+            </Stack.Screen>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{ flex: 1, padding: SIZES.medium }}>
+                    <Welcome></Welcome>
+                    <Popularjobs></Popularjobs>
+                    <Nearbyjobs></Nearbyjobs>
+                </View>
+            </ScrollView>
+
+        </SafeAreaView>
     )
 }
 
--- job-search-app/components/home/popular/Popularjobs.jsx
+++ job-search-app/components/home/popular/Popularjobs.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './popularjobs.style'
-
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
+import { useState } from 'react'
+import { useRouter } from 'expo-router'
+import { icons, SIZES } from '../../../constants';
+import styles from './popularjobs.style';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import { isLoading } from 'expo-font';
 const Popularjobs = () => {
+  const isLoading = false;
+  const error = false;
+  const router = useRouter();
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}>
+        {isLoading?(<ActivityIndicator></ActivityIndicator>):error?(<Text>Something went wrong!</Text>):<FlatList
+        data={[1,2,3,4,5]}
+        renderItem={({item})=>(
+          <PopularJobCard item={item}></PopularJobCard>
+        )}
+        keyExtractor={item => item?.job_id}
+        contentContainerStyle={{columnGap:SIZES.medium}}
+        horizontal
+        ></FlatList>}
+      </View>
     </View>
   )
 }
