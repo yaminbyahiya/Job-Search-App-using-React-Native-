8c985d4ced3d2fc2ad1b3a7ba1b72df66c316a8d
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-07-11 17:32:59 +0600

    Job Qualifications useFetch() using RapidAPI

--- job-search-app/app/job-details/[id].js
+++ job-search-app/app/job-details/[id].js
@@ -5,12 +5,33 @@ import { useState, useCallback } from 'react';
 import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from '../../components';
 import { COLORS, icons, SIZES } from '../../constants';
 import useFetch from '../../hook/useFetch';
+const tabs=["About", "Qualifications", "Responsibilities"];
 const JobDetails = () => {
     const params = useSearchParams();
+    console.log(params.id);
     const router = useRouter();
     const { data, isLoading, error, refetch } = useFetch('job-details', {
-        job_id: params.id
-    })
+        job_id: params.id,
+    });
+    console.log(data);
+    const [refreshing, setRefreshing] = useState(false);
+    const [activeTab, setActiveTab] = useState(tabs[0]);
+    const onRefresh = () => {}
+    const displayTabContent = () => {
+        switch (activeTab) {
+            case "Qualifications":
+                return <Specifics
+                title="Qualifications"
+                points={data[0].job_highlights?.Qualifications ?? ['N/A']}></Specifics>
+                break;
+            case "About":
+                break
+            case "Responsibilities":
+                break
+            default:
+                break;
+        }
+    }
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
             <Stack.Screen
@@ -29,10 +50,36 @@ const JobDetails = () => {
                             iconUrl={icons.share}
                             dimension={"60%"}
                             handlePress={() => router.back()}></ScreenHeaderBtn>
-                    )
+                    ),
+                    headerTitle:''
                 }}>
-
             </Stack.Screen>
+            <>
+                <ScrollView 
+                showsVerticalScrollIndicator={false} 
+                refreshControl={
+                    <RefreshControl 
+                    refreshing={refreshing} 
+                    onRefresh={onRefresh}>
+                    </RefreshControl>}>
+                        {isLoading?(
+                        <ActivityIndicator size="large" color={COLORS.primary}></ActivityIndicator>):error?(
+                        <Text>Something went wrong</Text>):data.length===0?(
+                        <Text>No data</Text>):(
+                        <View style={{padding:SIZES.medium, paddingBottom:100}}>
+                            <Company 
+                            companyLogo={data[0].employer_logo}
+                            jobTitle={data[0].job_title}
+                            companyName={data[0].employer_name}
+                            location={data[0].job_country}></Company>
+                            <JobTabs
+                            tabs={tabs}
+                            activeTab={activeTab}
+                            setActiveTab={setActiveTab}></JobTabs>
+                            {displayTabContent()}
+                        </View>)}
+                </ScrollView>
+            </>
         </SafeAreaView>
     )
 }
--- job-search-app/hook/useFetch.js
+++ job-search-app/hook/useFetch.js
@@ -5,7 +5,6 @@ const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-
   const options = {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
